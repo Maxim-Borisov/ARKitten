ad69b16ccc1a0c5af92376776bc988f7a9ce8e5d
LEGION <maxim-borisov@mail.com>
2018-09-14 02:09:48 +0300

    Added interface buttons in order to control animations

---
+++ Assets/AnimationManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour {
+    private Animation _animation;
+    private bool _inAction;
+ 
+    void Start () {
+        _animation = GetComponent<Animation>();
+	}
+
+    public void ResetAntimation()
+    {
+        _inAction = false;
+        _animation.Play("Idle");
+    }
+
+    public void Meow()
+    {
+        if (_inAction)
+        {
+            return;
+        }
+
+        _inAction = true;
+        _animation.Play("Meow");
+        Invoke("ResetAnimation", 1.10f);
+    }
+
+    public void Itch()
+    {
+        if(_inAction)
+        {
+            return;
+        }
+
+        _inAction = true;
+        _animation.Play("Ithcing");
+        Invoke("ResetAnimation", 1.30f);
+    }
+}
